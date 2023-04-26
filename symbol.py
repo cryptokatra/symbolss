b0c66011d89edf61bbb899c7d1b1c0faf107be0e
cryptokatra <129519216+cryptokatra@users.noreply.github.com>
2023-04-26 20:45:35 +0800

    Update symbol.py

--- symbol.py
+++ symbol.py
@@ -8,6 +8,12 @@ def add_prefix(text, prefix):
         prefix_list = [f"({i})" for i in range(1, len(lines) + 1)]
     elif prefix == "With[]":
         prefix_list = [f"[{i}]" for i in range(1, len(lines) + 1)]
+    elif prefix == "●":
+        prefix_list = ["●"] * len(lines)
+    elif prefix == "■":
+        prefix_list = ["■"] * len(lines)
+    elif prefix == "▶":
+        prefix_list = ["▶"] * len(lines)
     else:
         prefix_list = [prefix] * len(lines)
     return "\n".join([f"{prefix_list[i]} {lines[i]}" for i in range(len(lines))])
@@ -22,7 +28,7 @@ def main():
 
     # Column 2
     st.sidebar.markdown("# Prefix Options")
-    prefix_type = st.sidebar.selectbox("Select prefix type:", ["With dot", "With ()", "With[]", "Custom"])
+    prefix_type = st.sidebar.selectbox("Select prefix type:", ["With dot", "With ()", "With[]", "●", "■", "▶", "Custom"])
     
     if prefix_type == "Custom":
         prefix = st.sidebar.text_input("Enter custom prefix here:")
@@ -34,11 +40,13 @@ def main():
     output_text = add_prefix(text_input, prefix)
     text_output = st.text_area("Result", value=output_text, height=500)
 
-    col1, col2 = st.beta_columns(2)
+    col1, col2, col3 = st.columns(3)
     if col1.button("Copy"):
         st.experimental_set_query_params(text_output=text_output)
     if col2.button("Reset"):
         st.experimental_set_query_params(text_output="")
+    if col3.button("Clear"):
+        st.experimental_set_query_params(text_input="", text_output="")
     
 if __name__ == "__main__":
     main()
