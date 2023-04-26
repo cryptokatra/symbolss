99d082c6ffbaaa62e14e61f6ae809435da4cbef9
cryptokatra <129519216+cryptokatra@users.noreply.github.com>
2023-04-26 21:04:11 +0800

    Update symbol.py

--- symbol.py
+++ symbol.py
@@ -24,12 +24,12 @@ def main():
 
     # Column 1
     st.sidebar.markdown("# Input Text")
-    text_input = st.sidebar.text_area("Enter text here:")
+    text_input = st.sidebar.text_area("Enter text here:", height=500)
 
     # Column 2
     st.sidebar.markdown("# Prefix Options")
     prefix_type = st.sidebar.selectbox("Select prefix type:", ["With dot", "With ()", "With[]", "●", "■", "▶", "Custom"])
-    
+
     if prefix_type == "Custom":
         prefix = st.sidebar.text_input("Enter custom prefix here:")
     else:
@@ -40,13 +40,32 @@ def main():
     output_text = add_prefix(text_input, prefix)
     text_output = st.text_area("Result", value=output_text, height=500)
 
-    col1, col2, col3 = st.columns(3)
-    if col1.button("Copy"):
+    if st.button("Copy"):
         st.experimental_set_query_params(text_output=text_output)
-    if col2.button("Reset"):
-        st.experimental_set_query_params(text_output="")
-    if col3.button("Clear"):
+
+    if st.button("Clear"):
+        text_input = ""
+        text_output = ""
         st.experimental_set_query_params(text_input="", text_output="")
+        
+    st.write(" ")
     
+    if st.button("Add Prefix"):
+        output_text = add_prefix(text_input, prefix)
+
+    st.radio("Select prefix type:", options=["With dot", "With ()", "With[]", "●", "■", "▶", "Custom"], key="options")
+
+    if st.session_state.options == "Custom":
+        prefix = st.text_input("Enter custom prefix here:", key="custom-prefix")
+    else:
+        prefix = st.session_state.options
+
+    st.text_input("Enter text here:", value=text_input, key="input-text")
+
+    output_text = add_prefix(text_input, prefix)
+    st.code(output_text)
+
+    st.empty()
+
 if __name__ == "__main__":
     main()
