f777737098e79cf45dde43b93ec9de6d20459051
cryptokatra <129519216+cryptokatra@users.noreply.github.com>
2023-04-26 21:58:12 +0800

    Update symbol.py

--- symbol.py
+++ symbol.py
@@ -1,79 +1,45 @@
 import streamlit as st
 
-def add_prefix(text, prefix):
-    lines = text.split("\n")
-    output = ""
-    counter = 1
-    for line in lines:
+def add_prefix(input_text, prefix):
+    lines = input_text.split("\n")
+    prefix_lines = []
+    for i, line in enumerate(lines):
         line = line.strip()
-        if line != "":
-            output += f"{prefix}{counter}. {line}\n"
-            counter += 1
-    return output.strip()
+        if line:
+            prefix_line = f"{prefix}{line}"
+            prefix_lines.append(prefix_line)
+    return "\n".join(prefix_lines)
 
-def add_number_prefix(text):
-    return add_prefix(text, "")
+def main():
+    st.title("Add Prefix to Text")
 
-def add_dot_prefix(text):
-    return add_prefix(text, "●")
+    # Column 1
+    input_text = st.text_area("Enter Text")
 
-def add_parenthesis_prefix(text):
-    return add_prefix(text, "(%d)")
-
-def add_square_bracket_prefix(text):
-    return add_prefix(text, "[%d]")
-
-def add_custom_symbol_prefix(text, symbol):
-    return add_prefix(text, symbol)
-
-prefix_options = ["With dot", "With ()", "With[]", "●", "■", "▶", "Custom symbol"]
-symbol_options = ["%", "$", "@"]
-
-st.set_page_config(page_title="Text Prefixer", page_icon=":memo:", layout="wide")
-
-# Define layout
-col1, col2, col3 = st.columns([1, 2, 1])
-
-# Column 1: Text Input
-with col1:
-    st.write("### Text Input")
-    text_input = st.text_area("", height=400)
-
-# Column 2: Prefix Options
-with col2:
-    st.write("### Prefix Options")
-    prefix_choice = st.selectbox("Choose a prefix option", prefix_options)
-    if prefix_choice == "With dot":
-        prefix_func = add_dot_prefix
-    elif prefix_choice == "With ()":
-        prefix_func = add_parenthesis_prefix
-    elif prefix_choice == "With[]":
-        prefix_func = add_square_bracket_prefix
-    elif prefix_choice == "●":
-        prefix_func = add_custom_symbol_prefix(text_input, "●")
-    elif prefix_choice == "■":
-        prefix_func = add_custom_symbol_prefix(text_input, "■")
-    elif prefix_choice == "▶":
-        prefix_func = add_custom_symbol_prefix(text_input, "▶")
+    # Column 2
+    format_options = ["With dot", "With ()", "With []", "●", "■", "▶", "Custom"]
+    format_option = st.selectbox("Select Format", format_options)
+    if format_option == "Custom":
+        prefix = st.text_input("Enter Custom Prefix")
     else:
-        symbol_choice = st.selectbox("Choose a custom symbol", symbol_options)
-        prefix_func = add_custom_symbol_prefix(text_input, symbol_choice)
+        if format_option == "With dot":
+            prefix = "1."
+        elif format_option == "With ()":
+            prefix = "(1)"
+        elif format_option == "With []":
+            prefix = "[1]"
+        else:
+            prefix = format_option
 
-    custom_prefix = st.text_input("Enter a custom prefix", value="", max_chars=10)
-    if custom_prefix:
-        prefix_func = add_custom_symbol_prefix(text_input, custom_prefix)
-
-# Column 3: Text Output
-with col3:
-    st.write("### Text Output")
+    # Column 3
     if st.button("Add Prefix"):
-        output = prefix_func(text_input)
-        output = output.replace("\n\n", "\n").strip()
-        st.text_area("", value=output, height=400)
-    if st.button("Copy"):
-        st.write("Copied to Clipboard!")
-        st.write(output)
-    if st.button("Reset"):
-        st.experimental_rerun()
+        output_text = add_prefix(input_text, prefix)
+        st.text_area("Result", output_text)
 
-st.write("---")
+    if st.button("Copy"):
+        output_text = add_prefix(input_text, prefix)
+        st.write(output_text)
+        st.text_input("", value=output_text)
+
+if __name__ == "__main__":
+    main()
