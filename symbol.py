3f2e8db3c4bf1630d86dd7dad8412386e9413481
cryptokatra <129519216+cryptokatra@users.noreply.github.com>
2023-04-26 21:44:25 +0800

    Update symbol.py

--- symbol.py
+++ symbol.py
@@ -1,58 +1,79 @@
 import streamlit as st
 
-def add_prefix(input_text, prefix):
-    lines = input_text.split("\n")
-    output = []
-    count = 1
+def add_prefix(text, prefix):
+    lines = text.split("\n")
+    output = ""
+    counter = 1
     for line in lines:
         line = line.strip()
-        if line:
-            output.append(f"{prefix} {count}. {line}")
-            count += 1
-        else:
-            output.append(line)
-    return "\n".join(output)
+        if line != "":
+            output += f"{prefix}{counter}. {line}\n"
+            counter += 1
+    return output.strip()
 
-def get_prefix_style(prefix_style):
-    prefix = ""
-    if prefix_style == "With dot":
-        prefix = "."
-    elif prefix_style == "With ()":
-        prefix = "()"
-    elif prefix_style == "With[]":
-        prefix = "[]"
-    elif prefix_style == "●":
-        prefix = "●"
-    elif prefix_style == "■":
-        prefix = "■"
-    elif prefix_style == "▶":
-        prefix = "▶"
-    return prefix
+def add_number_prefix(text):
+    return add_prefix(text, "")
 
-st.set_page_config(page_title="Prefix Adder")
+def add_dot_prefix(text):
+    return add_prefix(text, "●")
 
-st.title("Add Prefix to Each Line of Text")
+def add_parenthesis_prefix(text):
+    return add_prefix(text, "(%d)")
 
-input_text = st.text_area("Input Text", "", height=300)
+def add_square_bracket_prefix(text):
+    return add_prefix(text, "[%d]")
 
-prefix_style = st.selectbox("Select Prefix Style", 
-    ["With dot", "With ()", "With[]", "●", "■", "▶"])
+def add_custom_symbol_prefix(text, symbol):
+    return add_prefix(text, symbol)
 
-custom_prefix = st.text_input("Custom Prefix", "")
+prefix_options = ["With dot", "With ()", "With[]", "●", "■", "▶", "Custom symbol"]
+symbol_options = ["%", "$", "@"]
 
-if custom_prefix:
-    prefix = custom_prefix
-else:
-    prefix = get_prefix_style(prefix_style)
+st.set_page_config(page_title="Text Prefixer", page_icon=":memo:", layout="wide")
 
-output_text = add_prefix(input_text, prefix)
+# Define layout
+col1, col2, col3 = st.columns([1, 2, 1])
 
-st.text_area("Output Text", output_text, height=300)
+# Column 1: Text Input
+with col1:
+    st.write("### Text Input")
+    text_input = st.text_area("", height=400)
 
-if st.button("Copy"):
-    st.write("Output text copied to clipboard!")
-    st.experimental_set_query_params(output=output_text)
+# Column 2: Prefix Options
+with col2:
+    st.write("### Prefix Options")
+    prefix_choice = st.selectbox("Choose a prefix option", prefix_options)
+    if prefix_choice == "With dot":
+        prefix_func = add_dot_prefix
+    elif prefix_choice == "With ()":
+        prefix_func = add_parenthesis_prefix
+    elif prefix_choice == "With[]":
+        prefix_func = add_square_bracket_prefix
+    elif prefix_choice == "●":
+        prefix_func = add_custom_symbol_prefix(text_input, "●")
+    elif prefix_choice == "■":
+        prefix_func = add_custom_symbol_prefix(text_input, "■")
+    elif prefix_choice == "▶":
+        prefix_func = add_custom_symbol_prefix(text_input, "▶")
+    else:
+        symbol_choice = st.selectbox("Choose a custom symbol", symbol_options)
+        prefix_func = add_custom_symbol_prefix(text_input, symbol_choice)
 
-if "output" in st.experimental_get_query_params():
-    copied_text = st.experimental_get_query_params()["output"]
-    st.text_area("Copied Text", copied_text, height=300)
+    custom_prefix = st.text_input("Enter a custom prefix", value="", max_chars=10)
+    if custom_prefix:
+        prefix_func = add_custom_symbol_prefix(text_input, custom_prefix)
+
+# Column 3: Text Output
+with col3:
+    st.write("### Text Output")
+    if st.button("Add Prefix"):
+        output = prefix_func(text_input)
+        output = output.replace("\n\n", "\n").strip()
+        st.text_area("", value=output, height=400)
+    if st.button("Copy"):
+        st.write("Copied to Clipboard!")
+        st.write(output)
+    if st.button("Reset"):
+        st.experimental_rerun()
+
+st.write("---")
