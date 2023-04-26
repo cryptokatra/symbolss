1f111709a47231055bd284416f737cf9ea0fd1ee
cryptokatra <129519216+cryptokatra@users.noreply.github.com>
2023-04-26 20:38:07 +0800

    Update symbol.py

--- symbol.py
+++ symbol.py
@@ -1,45 +1,44 @@
 import streamlit as st
 
 def add_prefix(text, prefix):
-    prefix_list = []
-    if prefix == "With dot  1.2.3.":
-        for i in range(1, len(text) + 1):
-            prefix_list.append(str(i) + ".")
-    elif prefix == "With () (1)(2)(3)":
-        for i in range(1, len(text) + 1):
-            prefix_list.append("(" + str(i) + ")")
-    elif prefix == "With[] [1][2][3]":
-        for i in range(1, len(text) + 1):
-            prefix_list.append("[" + str(i) + "]")
+    lines = text.split('\n')
+    if prefix == "With dot":
+        prefix_list = [f"{i}." for i in range(1, len(lines) + 1)]
+    elif prefix == "With ()":
+        prefix_list = [f"({i})" for i in range(1, len(lines) + 1)]
+    elif prefix == "With[]":
+        prefix_list = [f"[{i}]" for i in range(1, len(lines) + 1)]
     else:
-        prefix_list = [prefix] * len(text)
-    return [prefix_list[i] + text[i] for i in range(len(text))]
+        prefix_list = [prefix] * len(lines)
+    return "\n".join([f"{prefix_list[i]} {lines[i]}" for i in range(len(lines))])
 
-st.title("Add Prefix to Lines")
+def main():
+    st.set_page_config(page_title="Add Prefix App")
+    st.title("Add Prefix App")
 
-# Define columns
-col1, col2, col3 = st.beta_columns(3)
+    # Column 1
+    st.sidebar.markdown("# Input Text")
+    text_input = st.sidebar.text_area("Enter text here:")
 
-# Column 1: Text Input
-text_input = col1.text_area("Input Text:")
-
-# Column 2: Prefix Options and Input
-with col2.beta_form("options_form"):
-    prefix_type = st.selectbox("Select Prefix Type:", ["Numeric", "Symbolic"])
-    if prefix_type == "Numeric":
-        prefix = st.selectbox("Select Numeric Prefix Style:", ["With dot  1.2.3.", "With () (1)(2)(3)", "With[] [1][2][3]"])
-        text_input2 = st.text_input("Input Custom Numeric Prefix Style:")
+    # Column 2
+    st.sidebar.markdown("# Prefix Options")
+    prefix_type = st.sidebar.selectbox("Select prefix type:", ["With dot", "With ()", "With[]", "Custom"])
+    
+    if prefix_type == "Custom":
+        prefix = st.sidebar.text_input("Enter custom prefix here:")
     else:
-        prefix = st.selectbox("Select Symbolic Prefix Style:", ["●", "■", "▶"])
-        text_input2 = st.text_input("Input Custom Symbolic Prefix Style:")
+        prefix = prefix_type
 
-# Column 3: Processed Text and Buttons
-converted_text = add_prefix(text_input.split("\n"), prefix) if prefix_type == "Numeric" else [prefix + line for line in text_input.split("\n")]
-output_text = "\n".join(converted_text)
-col3.text_area("Output Text:", output_text)
-if col3.button("Copy"):
-    st.experimental_set_query_params(text=output_text)
-    st.success("Copied to Clipboard!")
-if col3.button("Reset"):
-    st.experimental_set_query_params()
-    st.experimental_rerun()
+    # Column 3
+    st.markdown("# Output Text")
+    output_text = add_prefix(text_input, prefix)
+    text_output = st.text_area("Result", value=output_text, height=500)
+
+    col1, col2 = st.beta_columns(2)
+    if col1.button("Copy"):
+        st.experimental_set_query_params(text_output=text_output)
+    if col2.button("Reset"):
+        st.experimental_set_query_params(text_output="")
+    
+if __name__ == "__main__":
+    main()
