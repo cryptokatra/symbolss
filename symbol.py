89f6a1ec7a902247124f8b290274818d964ec174
cryptokatra <129519216+cryptokatra@users.noreply.github.com>
2023-04-26 18:38:34 +0800

    Update symbol.py

--- symbol.py
+++ symbol.py
@@ -1,65 +1,52 @@
 import streamlit as st
 
-def main():
-    st.set_page_config(page_title="Prefix Tool", page_icon=":pencil2:")
-    st.title("Prefix Tool")
+def add_prefix(prefix, lines, style):
+    prefix_lines = []
+    if style == "With dot":
+        for i, line in enumerate(lines):
+            prefix_lines.append(f"{i+1}{prefix}{line}")
+    elif style == "With ()":
+        for i, line in enumerate(lines):
+            prefix_lines.append(f"({i+1}){prefix}{line}")
+    elif style == "With[]":
+        for i, line in enumerate(lines):
+            prefix_lines.append(f"[{i+1}]{prefix}{line}")
+    elif style == "Custom":
+        for i, line in enumerate(lines):
+            prefix_lines.append(f"{prefix}{line}")
+    return prefix_lines
 
-    # 文本框A
-    st.subheader("Input Text")
-    input_text = st.text_area("Enter text here", height=150)
+st.title("Add Prefix Tool")
 
-    # 文本框B + 選擇數字
-    st.subheader("Add Prefix Number")
-    prefix_num = st.selectbox("Select number style", options=["With dot 1.2.3.", "With () (1)(2)(3)", "With[] [1][2][3]", "Custom"], index=0)
-    if prefix_num == "Custom":
-        custom_num = st.text_input("Enter custom number style")
-        prefix_num = custom_num
+# Define the text input box for the user
+text_input = st.text_area("Enter Text Here:")
 
-    with_num = ""
-    if prefix_num.startswith("With dot"):
-        with_num = "1. "
-    elif prefix_num.startswith("With ()"):
-        with_num = "(1) "
-    elif prefix_num.startswith("With[]"):
-        with_num = "[1] "
-    else:
-        with_num = prefix_num + " "
+# Define the prefix style dropdown for the user
+prefix_style = st.selectbox("Select Prefix Style:", ("With dot", "With ()", "With[]", "Custom"))
 
-    text_with_prefix_num = ""
-    text_lines = input_text.split("\n")
-    for index, line in enumerate(text_lines):
-        text_with_prefix_num += f"{with_num}{line}\n"
+# Define the prefix input box for the user
+prefix_input = st.text_input("Enter Prefix Here:", "")
 
-    st.text_area("Text with Prefix Number", value=text_with_prefix_num, height=150)
+# Define the symbol style dropdown for the user
+symbol_style = st.selectbox("Select Symbol Style:", ("●", "■", "▶", "Custom"))
 
-    # 文本框C + 選擇符號
-    st.subheader("Add Prefix Symbol")
-    prefix_sym = st.selectbox("Select symbol style", options=["BLACK CIRCLE ●", "BLACK SQUARE ■", "BLACK RIGHT-POINTING TRIANGLE ▶", "Custom"], index=0)
-    if prefix_sym == "Custom":
-        custom_sym = st.text_input("Enter custom symbol style")
-        prefix_sym = custom_sym
+# Define the symbol input box for the user
+symbol_input = st.text_input("Enter Symbol Here:", "●")
 
-    text_with_prefix_sym = ""
-    text_lines = input_text.split("\n")
-    for index, line in enumerate(text_lines):
-        text_with_prefix_sym += f"{prefix_sym} {line}\n"
+# Define the buttons
+if st.button("Add Prefix"):
+    lines = text_input.split("\n")
+    prefix_lines = add_prefix(prefix_input, lines, prefix_style)
+    prefix_text = "\n".join(prefix_lines)
+    st.text_area("Prefixed Text:", value=prefix_text)
+    
+if st.button("Add Symbol"):
+    lines = text_input.split("\n")
+    symbol_lines = [f"{symbol_input} {line}" for line in lines]
+    symbol_text = "\n".join(symbol_lines)
+    st.text_area("Symbolized Text:", value=symbol_text)
 
-    st.text_area("Text with Prefix Symbol", value=text_with_prefix_sym, height=150)
-
-    # 清除所有
-    if st.button("Clear All"):
-        input_text = ""
-        text_with_prefix_num = ""
-        text_with_prefix_sym = ""
-
-    # 複製到剪貼板
-    if st.button("Copy Text with Prefix Number"):
-        st.write("Copied to clipboard!")
-        st.experimental_set_query_params(text_with_prefix_num=text_with_prefix_num)
-
-    if st.button("Copy Text with Prefix Symbol"):
-        st.write("Copied to clipboard!")
-        st.experimental_set_query_params(text_with_prefix_sym=text_with_prefix_sym)
-
-if __name__ == "__main__":
-    main()
+if st.button("Clear All"):
+    st.text_input("Enter Text Here:", value="")
+    st.text_input("Enter Prefix Here:", value="")
+    st.text_input("Enter Symbol Here:", value="")
